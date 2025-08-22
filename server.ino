ba374744647e0975ca475d5302f2b13d440e7d20
Kyrul-com <kyrul674@gmail.com>
2025-08-22 13:21:40 +0800

    Update server.ino

--- server.ino
+++ server.ino
@@ -2,6 +2,7 @@
 #include <WebSocketsServer.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
+#include <esp_now.h>
 
 // ==== CONFIG ====
 const char* ssid = "Khairul's iPhone";
@@ -13,67 +14,82 @@ const int controlPin = 16;
 AsyncWebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
+// Latest sensor values
+float latestTemp = 0.0;
+float latestHum  = 0.0;
+
+// ==== ESP-NOW MESSAGE STRUCT ====
+typedef struct struct_message {
+  float temp;
+  float hum;
+} struct_message;
+
+struct_message incomingData;
+
+// ==== ESP-NOW CALLBACK ====
+void onDataRecv(const uint8_t * mac, const uint8_t *incomingDataBytes, int len) {
+  memcpy(&incomingData, incomingDataBytes, sizeof(incomingData));
+  latestTemp = incomingData.temp;
+  latestHum = incomingData.hum;
+
+  Serial.printf("Received: Temp=%.1fC Hum=%.1f%%\n", latestTemp, latestHum);
+
+  String data = "TEMP:" + String(latestTemp,1) + "C HUM:" + String(latestHum,1) + "%";
+  webSocket.broadcastTXT(data);
+}
+
 // ==== WEBSOCKET EVENTS ====
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-switch(type) {
-case WStype_DISCONNECTED:
-Serial.printf("[%u] Disconnected!\n", num);
-break;
+  if (type == WStype_TEXT) {
+    String msg = String((char*)payload);
+    Serial.printf("Client[%u] says: %s\n", num, msg.c_str());
 
-case WStype_CONNECTED:  
-  Serial.printf("[%u] Connected!\n", num);  
-  break;  
-
-case WStype_TEXT:  
-  String msg = String((char*)payload);  
-  Serial.printf("[%u] Message: %s\n", num, msg.c_str());  
-
-  if (msg == "ON") {  
-    digitalWrite(controlPin, HIGH);  
-    webSocket.broadcastTXT("Pin16:ON");  
-  } else if (msg == "OFF") {  
-    digitalWrite(controlPin, LOW);  
-    webSocket.broadcastTXT("Pin16:OFF");  
-  }  
-  break;
-
-}
+    if (msg == "ON") {
+      digitalWrite(controlPin, HIGH);
+      webSocket.broadcastTXT("Pin16:ON");
+    } else if (msg == "OFF") {
+      digitalWrite(controlPin, LOW);
+      webSocket.broadcastTXT("Pin16:OFF");
+    }
+  }
 }
 
 // ==== SETUP ====
 void setup() {
-Serial.begin(115200);
+  Serial.begin(115200);
 
-pinMode(controlPin, OUTPUT);
-digitalWrite(controlPin, LOW);
+  pinMode(controlPin, OUTPUT);
+  digitalWrite(controlPin, LOW);
 
-if(!SPIFFS.begin(true)){
-Serial.println("An Error has occurred while mounting SPIFFS");
-return;
-}
+  if(!SPIFFS.begin(true)){
+    Serial.println("Error mounting SPIFFS");
+    return;
+  }
 
-WiFi.begin(ssid, password);
-Serial.print("Connecting to WiFi");
-while (WiFi.status() != WL_CONNECTED) {
-delay(500);
-Serial.print(".");
-}
-Serial.println();
-Serial.print("IP Address: ");
-Serial.println(WiFi.localIP());
+  WiFi.mode(WIFI_AP_STA);  // allows ESP-NOW + WiFi at the same time
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.println();
+  Serial.print("Server IP: "); Serial.println(WiFi.localIP());
 
-// Serve HTML file
-server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-request->send(SPIFFS, "/index.html", "text/html");
-});
+  // ESP-NOW init
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_recv_cb(onDataRecv);
 
-server.begin();
-webSocket.begin();
-webSocket.onEvent(webSocketEvent);
+  // Serve HTML
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 }
 
 // ==== LOOP ====
 void loop() {
-webSocket.loop();
+  webSocket.loop();
 }
-
